ee17ef3595010392fd94fb29a7f442b4e0b27f9d
jose <josloe@gmail.com>
2020-01-29 19:20:56 +0100

    fixed serverlive reload

--- src/serverlivereload.py
+++ src/serverlivereload.py
@@ -5,7 +5,7 @@ LIVE RELOAD
 
 import logging
 from os import getcwd, path
-from formic import FileSet
+# from formic import FileSet
 from livereload import Server, shell
 from flask import Flask
 
@@ -13,49 +13,30 @@ STATIC_FOLDER = "."
 
 
 ##### routes
-app = Flask(__name__,
-            static_folder=STATIC_FOLDER,
-            static_url_path="")
+# app = Flask(__name__,
+#             static_folder=STATIC_FOLDER,
+#             static_url_path="")
 
 
-@app.route("/")
-def root():
-    return app.send_static_file("index.html")
+app = Flask(__name__, instance_relative_config=True)
+
+
+# @app.route("/")
+# def root():
+#     return app.send_static_file("index.html")
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 #####################
 
 
 #### servidor #################
 
-def make_livereload_server(wsgi_app):
-    server = Server(wsgi_app)
-
-    watch_patterns = (
-        "/src/**",
-        "/static/**"
-    )
-
-    build_cmd = "make"
-
-    print ("Files being monitored:")
-
-    cwd = getcwd()
-
-    for pattern in watch_patterns:
-        print ("Pattern: {0}".format(pattern))
-        for filepath in FileSet(include=pattern):
-            print ("=>".format(path.relpath(filepath, cwd)))
-            server.watch(filepath, build_cmd)
-        print
-
-    return server
-
-
-def main():
-    flask_wsgi_app = app.wsgi_app
-    server = make_livereload_server(flask_wsgi_app)
-    server.serve()
 
 
 if __name__ == "__main__":
-    main()
+    server = Server(app.wsgi_app)
+    server.serve()
